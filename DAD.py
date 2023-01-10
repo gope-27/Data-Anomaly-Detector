afa18b836e718b247eb2d43c1961e356d4df4c40
gope-27 <117707851+gope-27@users.noreply.github.com>
2023-01-10 16:29:03 +0530

    Update DAD.py

--- DAD.py
+++ DAD.py
@@ -7,14 +7,6 @@ from datetime import datetime
 import sqlalchemy
 from sqlalchemy import create_engine
 from annotated_text import annotated_text
-# Import required modules
-
-# from pathlib import Path
-# import streamlit_authenticator as stauth
-# import pandas as pd
-# import streamlit as st
-# from PIL import Image
-# from annotated_text import annotated_text
 
 
 st.set_page_config(page_title="DAD", page_icon="tata-icon (1).png",
@@ -50,7 +42,8 @@ annotated_text(
  )
 
 
-# st.subheader("Data Anomaly Detector")
+
+st.subheader("Data Anomaly Detector")
 
 name = st.text_input("Enter your name ")
 
@@ -66,7 +59,8 @@ ptx_columns_number = 21
 data_file_name = st.sidebar.selectbox('Select the Name of the Data File', ['PTX', 'Cosmetics'])
 
 if data_file_name == 'PTX':
-    file_uploaded = st.file_uploader("Upload Dataset"+ data_file_name, type=["csv", "xlsx", "xls"])
+
+    file_uploaded = st.file_uploader("Upload Dataset " + data_file_name, type=["csv", "xlsx", "xls"])
 
     if file_uploaded is not None:
         st.success('File Uploaded Successfully!')
@@ -91,12 +85,13 @@ if data_file_name == 'PTX':
             else:
                 st.write(col +' ---> Column not Available')
 
-       
+        new_columns = []
 
         if np.nan in list(input_df['Date'].unique()):
             st.error('Repeated Data Columns/Found Null/None/Blank in Data Columns', icon="🚨")
-        
+
         new_columns = []
+
         found = False
         for col in input_df.columns:
             if col not in ptx_columns:
@@ -107,9 +102,6 @@ if data_file_name == 'PTX':
             new_columns
         else:
             st.write("")
-
-        # new_columns
-        
 
 
         file_details = {"Filename": file_uploaded.name,
@@ -123,13 +115,6 @@ if data_file_name == 'PTX':
         
         df_data = [name,filename,filetype,current_dateTime,len(input_df.columns)]
         
-
-        # df_data = pd.DataFrame()
-        # df = df.append({'Name':name},ignore_index = True)
-        # df = df.append({"Filename":filename},ignore_index = True)
-        # df = df.append({"FileType":filetype },ignore_index = True)
-        # df_data.append(df_data, ignore_index=True)
-
         df_cols = ['Name', 'Filename' , 'Filetype','Date & Time','Column_Count']
         
         new_df =pd.DataFrame(columns = df_cols)
@@ -140,7 +125,7 @@ if data_file_name == 'PTX':
         new_df
 
         DB = {'servername': 'DESKTOP-GARLJAI\SQLEXPRESS',
-            'database': 'DATA',
+            'database': 'PTX',
             'driver': 'driver=ODBC Driver 17 for SQL Server'}
 
           # create the connection
@@ -157,5 +142,97 @@ if data_file_name == 'PTX':
         else:
             st.warning('Columns not matching', icon="⚠️")
 
-    
+# PTX Anamoly Detector Module
+cosmetics_columns = ['Band 2D Barcode', 'Work Order', 'Station', 'Date & Time', 'Shift',
+       'Batch', 'Inspector', 'Line No.', 'Status', 'Defect', 'Grade',
+       'Locations']
 
+cosmetics_columns_number = 12
+
+
+if data_file_name == 'Cosmetics':
+
+    
+    file_uploaded = st.file_uploader("Upload Dataset " + data_file_name, type=["csv", "xlsx", "xls"])
+
+    if file_uploaded is not None:
+        st.success('File Uploaded Successfully!')
+        input_df = pd.read_excel(file_uploaded)   
+
+        # Performing Minimal Data Cleaning
+        input_df.dropna(how = 'all', axis = 0, inplace = True)
+
+        # Checking Column Count in Uploaded File
+        if len(input_df.columns) == cosmetics_columns_number:
+            st.write('Requied Column Count: '+ str(cosmetics_columns_number),icon =  '✅')
+            st.write('Uploaded Column Count:'+ str(len(input_df.columns)))
+            st.info('Column Count is matching with uploaded file')
+        else:
+            st.write('Requied Column Count: '+str(cosmetics_columns_number), icon = '✅')
+            st.write('Uploaded Column Count: '+str(len(input_df.columns)))
+            st.info('Column Count is not matching')
+
+        for col in cosmetics_columns:
+            if col in input_df.columns:
+                print(col + ' ---> Column Availble')
+            else:
+                st.write(col +' ---> Column not Available')
+
+        new_columns = []
+
+        if np.nan in list(input_df['Date & Time'].unique()):
+            st.error('Repeated Data Columns/Found Null/None/Blank in Data Columns', icon="🚨")
+
+        new_columns = []
+
+        found = False
+        for col in input_df.columns:
+            if col not in ptx_columns:
+                new_columns.append(col)
+                found = True
+        if found:
+            st.write("Below columns are not in the given list")
+        else:
+            st.write("")
+
+        new_columns
+        
+        file_details = {"Filename": file_uploaded.name,
+                    "FileType": file_uploaded.type, "FileSize": file_uploaded.size}
+
+        filename = file_details["Filename"]
+
+        filetype = file_details['FileType']
+       
+        current_dateTime = datetime.now()
+        
+        df_data = [name,filename,filetype,current_dateTime,len(input_df.columns)]
+
+        df_cols = ['Name', 'Filename' , 'Filetype','Date & Time','Column_Count']
+        
+        new_df =pd.DataFrame(columns = df_cols)
+
+        # using the loc indexer
+        new_df.loc[0] = df_data
+
+        new_df
+
+        DB = {'servername': 'DESKTOP-GARLJAI\SQLEXPRESS',
+            'database': 'Cosmetics',
+            'driver': 'driver=ODBC Driver 17 for SQL Server'}
+
+          # create the connection
+        engine = create_engine(
+           'mssql+pyodbc://' + DB['servername'] + '/' + DB['database'] + "?" + DB['driver'])
+
+        data2 = new_df.to_sql(
+                'DAD', con=engine, index=False, if_exists='append')
+
+        if len(input_df.columns) == cosmetics_columns_number:
+
+            st.download_button(label='Download csv',
+                           data=new_df.to_csv(), mime='text/csv')
+        else:
+            st.warning('Columns not matching', icon="⚠️")
+
+        
